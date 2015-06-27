284fac378c2cf48d93ee06259a21ef1e098d7a13
WindWen <1124926655@qq.com>
2015-06-27 13:20:41 +0800

    Add private keys

---
+++ private/crypto.js
@@ -0,0 +1,16 @@
+/**
+ * Created by King on 2015/5/29.
+ * Crypto config
+ */
+(function(){
+
+    "use strict";
+
+    module.exports = {
+        "key":"algorithm",
+        "algorithm":"aes-128-ecb",
+        "cipherEncode":"hex",
+        "cipherDecode":"utf8"
+    };
+
+}).call(this);
---
+++ private/oss.js
@@ -0,0 +1,10 @@
+/**
+ * Created by King on 2015/6/24.
+ * Oss key
+ */
+(function(){
+    "use strict";
+
+    module.exports = "VOd01zYYBj1hAOAAYZgHremfuvI=";
+
+}).call(this);
