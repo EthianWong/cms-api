01feb06cebb936b558e15e02ef67df71d308d90f
WindWen <1124926655@qq.com>
2015-06-27 13:14:26 +0800

    Add plate DAO

---
+++ services/plate.js
@@ -0,0 +1,64 @@
+/**
+ * Created by King on 2015/6/3.
+ * Plate DAO
+ */
+(function(){
+
+    "use strict";
+
+    var Database = require('../conf/database');
+    var Plate = require('../models/plate')(Database.connection);
+
+    module.exports.create = function(params,callback){
+
+        Plate.create(params,function(err,result){
+            err ? callback(err, null) : callback(null, result);
+        });
+    };
+
+    module.exports.save = function(params,callback){
+
+        Plate.findOneAndUpdate({_id:params._id},params,{new: true},function(err,result){
+            err ? callback(err, null) : callback(null, result);
+        });
+    };
+
+    var _query = function(params){
+
+        var query = Plate.find({});
+
+        if(params.isVisible){
+            query.where('isVisible',params.isVisible);
+        }
+
+        if(params.name){
+            var reg = new RegExp(params.name);
+            query.find({"$or" : [ {"zh_name":reg} , {"en_name":reg} ] });
+        }
+
+        return query;
+    };
+
+    module.exports.select = function(params,callback){
+
+        var perPage = params["per-page"],pageIndex = params["page"] - 1;
+
+        var query = _query(params);
+
+        query.limit(perPage).skip(perPage * pageIndex).sort({'create_time':1});
+
+        query.exec(function(err,result){
+            err ? callback(err, null) : callback(null, result);
+        })
+    };
+
+    module.exports.count = function(params,callback){
+
+        var query = _query(params).count();
+
+        query.exec(function(err,result){
+            err ? callback(err, null) : callback(null, result);
+        })
+    };
+
+}).call(this);
