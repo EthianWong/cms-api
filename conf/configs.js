03b6f80e658687f57df1cedeb2a758fed3b64fc3
WindWen <1124926655@qq.com>
2015-07-29 15:03:00 +0800

    token's expires setting

---
+++ conf/configs.js
@@ -0,0 +1,17 @@
+/**
+ * Created by dell on 2015/6/18.
+ */
+(function(){
+
+    "use strict";
+
+    module.exports = {
+        isDeveloper:true,
+        log:{
+            path:"../log/",
+            name:"error.log"
+        },
+        expires:0.5
+    };
+
+}).call(this);
